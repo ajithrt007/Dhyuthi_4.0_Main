8b9a1ee77b5e86fc89f06d48461011bb19450669
ajithrt007 <ajithrt170@gmail.com>
2022-10-12 18:07:55 +0530

    poster loader test

--- static/js/event-poster-loader.js
+++ static/js/event-poster-loader.js
@@ -1,9 +1,9 @@
-const eventPoster = document.getElementsByClassName("classForJs");
-const noOfEventPosters = eventPoster.length;
+const noOfEventPosters = document.getElementsByClassName("classForJs").length;
 var i;
 for (i=0;i<noOfEventPosters;i++){
-    eventPoster[i].addEventListener("load",function(){
-        eventPoster[i].style.opacity = "1";
+    console.log(i + "not loaded");
+    document.getElementsByClassName("classForJs")[i].addEventListener("load",function(){
+        document.getElementsByClassName("classForJs")[i].style.opacity = "1";
         console.log(i + "is loaded");
     });
 }
