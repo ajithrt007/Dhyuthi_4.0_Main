99ac82dd3a773b3ddca21b5d2ed82ac4235ce700
ajithrt007 <ajithrt170@gmail.com>
2022-10-13 09:39:04 +0530

    Gallery Style added 1

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,10 +1,37 @@
 const galleryImg = document.getElementsByTagName("figcaption");
-galleryImg.addEventListener("mouseover",function(){
 
-});
-galleryImg.addEventListener("mouseover",function(){
+for(var i = 0;i<9;i++){
+    galleryImg[i].addEventListener("mouseover",function(){
+        if(screen.width > 820 ){
+            this.style.opacity = "0";
+            this.parentElement.children[1].classList.add("animatedPicture");
+            this.parentElement.children[1].scrollIntoView({behavior: "smooth", block: "center", inline: "center"});  
+        }
+    });
+}
 
-});
-galleryImg.addEventListener("mouseover",function(){
-
-});
+for(var i = 0;i<9;i++){
+    galleryImg[i].addEventListener("mouseout",function(){
+        if(screen.width > 820 ){
+            this.style.opacity = "1";  
+            this.parentElement.children[1].classList.remove("animatedPicture");  
+        }
+    });
+}
+var flagArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+for(var i = 0;i<9;i++){
+    galleryImg[i].addEventListener("click",function(){
+        if(screen.width < 820 ){
+            if(flagArray[i] == 0){                
+                this.style.opacity = "0";
+                this.parentElement.children[1].classList.add("animatedPicture");  
+                flagArray[i] = 1;
+            }
+            else{
+                this.style.opacity = "1";
+                this.parentElement.children[1].classList.remove("animatedPicture"); 
+                flagArray[i] = 0; 
+            }
+        }
+    });
+}
