9cb822b85d624c7f5ba8108d6686d32c03d46df2
ajithrt007 <ajithrt170@gmail.com>
2022-10-13 08:33:56 +0530

    Gallery Style added

---
+++ static/js/gallery.js
@@ -0,0 +1,10 @@
+const galleryImg = document.getElementsByTagName("figcaption");
+galleryImg.addEventListener("mouseover",function(){
+
+});
+galleryImg.addEventListener("mouseover",function(){
+
+});
+galleryImg.addEventListener("mouseover",function(){
+
+});
