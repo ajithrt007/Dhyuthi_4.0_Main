9201e9281ca33e646aacf7a0f800855b500499bc
ajithrt007 <ajithrt170@gmail.com>
2022-10-16 08:44:25 +0530

    Added loader

---
+++ static/js/preloader.js
@@ -0,0 +1,12 @@
+var scroll_y;
+var body = document.getElementsByTagName("body")[0];
+var loader = document.querySelector(".page-loader");
+window.addEventListener("scroll", function(event) {  
+    var scroll_y = this.scrollY;
+    loader.style.top = scroll_y + 'px';
+});
+body.style.overflowY = "hidden";
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+    body.style.overflowY = "unset";
+});
